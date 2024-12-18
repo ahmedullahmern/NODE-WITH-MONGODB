c23a868ae25aeb0a404649e1b8347e809350293c
ahmedullahmern <ahmedullahmern@gmail.com>
2024-12-19 01:01:35 +0500

    task get post dynamicget put delete done

--- routers/task.js
+++ routers/task.js
@@ -12,8 +12,34 @@ router.post("/", async (req, res) => {
 
 
 router.get("/", async (req, res) => {
-    const tasks = Task.find();
+    const tasks = await Task.find();
     sendResponse(res, 200, tasks, "Task fetched Successfully")
+})
+
+
+router.get("/:id", async (req, res) => {
+    const task = await Task.findById(req.params.id)
+    if (!task)
+        sendResponse(res, 404, null, "Task not found")
+    sendResponse(res, 200, task, "Task fetched Successfully")
+})
+
+router.put("/:id", async (req, res) => {
+    const { task, completed } = req.body
+    const taskFromDB = await Task.findById(req.params.id)
+    if (!taskFromDB) return sendResponse(res, 404, null, "Task not found")
+    if (task) taskFromDB.task = task
+    if (completed) taskFromDB.completed = completed
+    await taskFromDB.save()
+    sendResponse(res, 200, taskFromDB, "Task updated Successfully")
+})
+
+
+router.delete("/:id", async (req, res) => {
+    const taskFromDB = await Task.findById(req.params.id)
+    if (!taskFromDB) return sendResponse(res, 404, null, "Task not found")
+    await Task.deleteOne({ _id: req.params.id })
+    sendResponse(res, 200, null, "Task Deleted Successfully")
 })
 
 export default router
