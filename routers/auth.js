3be983cdffadda8f2a0c322f425fa72b03c77068
ahmedullahmern <ahmedullahmern@gmail.com>
2024-12-23 23:33:26 +0500

    user Register Function Completed using bcrypt

--- routers/auth.js
+++ routers/auth.js
@@ -6,18 +6,22 @@ import sendResponse from '../helpers/sendResponse.js';
 const router = express.Router()
 
 const registerSchema = Joi.object({
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: Joi.string().min(6),
     fullname: Joi.string().alphanum().min(3).max(30).required(),
 });
 
 
-router.post("/register", (req, res) => {
-    const { error, value } = registerSchema.validate({ a: 'a string' });
-    console.log("error==>", error)
-    if (error) return sendResponse(res, 400, null, true, "Please input is valid")
-    console.log("value==>", value);
-    res.send("Working On Register API")
+router.post("/register", async (req, res) => {
+    const { error, value } = registerSchema.validate(req.body);
+    if (error) return sendResponse(res, 400, null, true, error.message)
+    const user = await User.findOne({ email: value.email })
+    if (user) return sendResponse(res, 403, null, true, "User With This Email already Exist")
+    const hashedPassword = await bcrypt.hash(value.password, 12)
+    value.password = hashedPassword;
+    let newUser = new User({ ...value });
+    newUser = await newUser.save()
+    sendResponse(res, 201, newUser, false,"User Register successfully")
 })
 router.post("/login", (req, res) => { })
 
